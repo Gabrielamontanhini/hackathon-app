f95e02f98fe74e113dfed5ada83c8aa6e59582f9
gabriela <gabrielamontanhini@gmail.com>
2023-10-12 19:04:24 -0300

    Feat: usercontext

--- App.js
+++ App.js
@@ -1,14 +1,19 @@
 import { StatusBar } from 'expo-status-bar';
 import { StyleSheet, Text, View } from 'react-native';
 import Bottom from './src/components/BottomComponent/bottom';
+import UserProvider from './src/context/UserContext/usercontext';
 
 export default function App() {
+  const lsSessao = JSON.parse(localStorage.getItem("sessao"))
+
   return (
+    <UserProvider>
     <View style={styles.container}>
       <Text>Open up App.js to start working on your app!</Text>
      
       <StatusBar style="auto" />
     </View>
+    </UserProvider>
   );
 }
 
