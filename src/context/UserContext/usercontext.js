0867437bdb4a47eade6b93f915c71fc7c80f2005
gabriela <gabrielamontanhini@gmail.com>
2023-10-13 19:09:37 -0300

    Refactor: storage no utils

--- src/context/UserContext/usercontext.js
+++ src/context/UserContext/usercontext.js
@@ -1,13 +1,13 @@
 import AsyncStorage from "@react-native-async-storage/async-storage"
-import { createContext, useState } from "react";
+import React , { createContext, useState } from "react";
 
  const UserContext = createContext()
 
-  function UserProvider({ children }) {
-const lsSessao = JSON.parse(AsyncStorage.getItem("sessao"))
-    const [sessao, setSessao] = useState(lsSessao)
+  function UserProvider({ children }) { //ESSA FUNÇÃO DEVE SER CHAMADA COMO FILHA DE NAVIGATION CONTAINER E DEVE ENGLOBAR ROUTES
+    const lsSessao = JSON.parse(AsyncStorage.getItem("sessao")) //PEGA O TOKEN NO STORAGE
+    const [sessao, setSessao] = useState(lsSessao) //USA O TOKEN
     return (
-        <UserContext.Provider value={{ sessao, setSessao }}>
+        <UserContext.Provider value={{ sessao, setSessao }}> 
             {children}
         </UserContext.Provider>
     )
