6fb15704a49ff51c71061dbd7b0b449e34ddda29
gabriela <gabrielamontanhini@gmail.com>
2023-10-13 12:26:59 -0300

    Feat: pagina inicial

--- src/pages/Inicial/inicial.js
+++ src/pages/Inicial/inicial.js
@@ -2,23 +2,52 @@ import { BigHeader } from "../../components/HeaderComponents/bigHeader";
 import { StyledSafeArea } from "../../styles/styledSafeArea";
 import { Card } from "../../components/CardComponent/card";
 import Bottom from "../../components/BottomComponent/bottom";
-import { TouchableOpacity } from "react-native";
+import { Text, TouchableOpacity } from "react-native";
+import { OptionsView } from "../../styles/styledOptions";
+import styled from "styled-components";
 
 
 export default function Inicial({ navigation }) {
     return (
         <StyledSafeArea>
             <BigHeader />
-                <TouchableOpacity onPress={()=>navigation.navigate('areadoagricultor')}>
-                    <Card  data={{ label: 'Área do Agricultor' }} />
-                </TouchableOpacity>
-                <TouchableOpacity onPress={()=>navigation.navigate('noticias')}>
-                    <Card data={{ label: 'Notícias' }} />
-                </TouchableOpacity>
-                <TouchableOpacity onPress={()=>navigation.navigate('capacitação')}>
-                    <Card data={{ label: 'Capacitação' }} />
-                </TouchableOpacity>
+            <OptionsView>
+                <InicialText>
+                    <Texto>O Aplicativo ABC++ tem como objetivo unir os produtores de culturas da Amazonia com as práticas de agricultura
+                    de baixo carbono. Veja notícias sobre o ABC+, crie e maneje sua conta com suas produções e veja instruções de como aderir da melhor maneira
+                    a praticas de baixo carbono.
+                    </Texto>
+                    <Texto>
+                      Crie ou entre na conta conta na Área do Agricultor
+                    </Texto>
+                </InicialText>
+            </OptionsView>
+
+            <TouchableOpacity onPress={() => navigation.navigate('areadoagricultor')}>
+                <Card data={{ label: 'Área do Agricultor' }} />
+            </TouchableOpacity>
+            <TouchableOpacity onPress={() => navigation.navigate('noticias')}>
+                <Card data={{ label: 'Notícias' }} />
+            </TouchableOpacity>
+            <TouchableOpacity onPress={() => navigation.navigate('capacitação')}>
+                <Card data={{ label: 'Capacitação' }} />
+            </TouchableOpacity>
             <Bottom navigation={navigation} />
         </StyledSafeArea>
     )
 }
+
+const InicialText = styled.View`
+width: 90%;
+height: fit-content;
+display: flex;
+flex-direction: column;
+justify-content: center;
+`
+
+const Texto =styled.Text`
+font-size: 18px;
+padding-top: 5%;
+padding-bottom: 5%;
+`
+
--- src/styles/styledOptions.js
+++ src/styles/styledOptions.js
@@ -5,6 +5,7 @@ export const OptionsView = styled.View`
 background-color: ${colors.secondColor};
 padding-left: 5%;
 padding-right: 5%;
+margin-top: 5%;
 box-sizing: border-box;
 border: 2px solid ${colors.bordersColor};
  border-radius: 10px;
