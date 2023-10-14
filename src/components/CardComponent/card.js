eceff92af8ade89bc80ed15e788c8e6b2fbd31b6
m4arthu <luisarthir259@gmail.com>
2023-10-13 21:33:36 -0300

    feat: inicial-page

--- src/components/CardComponent/card.js
+++ src/components/CardComponent/card.js
@@ -1,5 +1,5 @@
 import { Text } from "react-native";
-import { StyledCard, TitleCard,CardIcon } from "./styled";
+import { StyledCard, TitleCard,CardIcon, TitleCardText } from "./styled";
 import { FontAwesomeIcon } from "@fortawesome/react-native-fontawesome";
 import { colors } from "../../constants/colors/colors";
 import { faChevronRight } from "@fortawesome/free-solid-svg-icons";
@@ -8,15 +8,13 @@ import { faChevronRight } from "@fortawesome/free-solid-svg-icons";
 export function Card({ data }) {
 
     return (
-        <Text>
             <StyledCard>
-                <TitleCard>{data.label}</TitleCard>
+                <TitleCard><TitleCardText>{data.label}</TitleCardText></TitleCard>
                 <CardIcon>
-                    <FontAwesomeIcon icon={faChevronRight} size={24} color={colors.bordersColor} />
+                    <FontAwesomeIcon icon={faChevronRight} size={24} color={colors.nonSelectedIcon} />
                 </CardIcon>
             </StyledCard>
-            {data.name}
-        </Text>
+       
     )
 }
 
